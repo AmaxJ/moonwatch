2dddcd0df971b9cac3f1f7317deec20458c9e584
Alan <ajohnson88@gmail.com>
2017-12-09 16:43:45 -0500

    real time chart updates

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/self-closing-comp */
+/* eslint-disable react/self-closing-comp, no-param-reassign */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Chart as ChartJS } from 'chart.js';
@@ -10,15 +10,25 @@ export default class Chart extends React.Component {
     static propTypes = {
         canvasNotSupportedText: PropTypes.string,
         height: PropTypes.number,
-        width: PropTypes.number,
-        priceHistory: PropTypes.array
+        interval: PropTypes.number,
+        lastPrice: PropTypes.object,
+        priceHistory: PropTypes.array,
+        width: PropTypes.number
+    }
+
+    static defaultProps = {
+        interval: 10
     }
 
     constructor(props) {
         super(props);
-        this.state = {
-            priceData: props.priceHistory
-        };
+        this.priceData = props.priceHistory.map((priceObj) => priceObj.close);
+        this.timestamps = props.priceHistory.map((priceObj) => {
+            return {
+                formattedTime: formatTime(priceObj.time),
+                time: priceObj.time
+            };
+        });
     }
 
     componentDidMount() {
@@ -38,14 +48,10 @@ export default class Chart extends React.Component {
         this.chart = new ChartJS(this.ctx, {
             type: 'line',
             data: {
-                labels: this.state.priceData.map((priceObj) => {
-                    const time = new Date(priceObj.time * 1000);
-                    // TODO if the time is 12:30 it shows as 12:3
-                    return `${time.getHours()}:${time.getMinutes()}`;
-                }),
+                labels: this.timestamps.map((timestamp) => timestamp.formattedTime),
                 datasets: [{
                     label: 'Price', // hidden
-                    data: this.state.priceData.map((priceObj) => priceObj.close),
+                    data: this.priceData,
                     backgroundColor: 'rgba(0, 0, 0, 0)',
                     borderColor: Colors.WHITE,
                     borderWidth: 0.8,
@@ -70,7 +76,28 @@ export default class Chart extends React.Component {
                 }
             }
         });
+    }
 
+    componentWillReceiveProps(nextProps) {
+        const { lastPrice: { close, time }, interval } = nextProps;
+        const secondsSinceRender = time - this.timestamps[this.timestamps.length - 1].time;
+        if (secondsSinceRender < interval) {
+            return;
+        }
+        this.priceData = this.priceData.concat([close]).slice(1);
+        this.timestamps = this.timestamps.concat(
+            [{ time, formattedTime: formatTime(time) }]
+        ).slice(1);
+
+        this.chart.data.labels = this.timestamps.map((timeObj) => timeObj.formattedTime);
+
+        this.chart.data.datasets.forEach((dataset) => {
+            dataset.data = this.priceData;
+        });
+
+        this.chart.update({
+            duration: 0
+        });
     }
 
     canvasRefHandler = (el) => {
@@ -90,4 +117,17 @@ export default class Chart extends React.Component {
             </div>
         );
     }
+}
+
+function formatTime(UTCTimestamp) {
+    const time = new Date(UTCTimestamp * 1000);
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const standardHour = hour > 12 ? hour - 12 : hour;
+    const formattedHour = standardHour === 0 ? 12 : standardHour;
+    const formattedMinute = minute === 0 ? '00' : minute;
+    if (formattedMinute < 10 && formattedMinute > 0) {
+        return `${formattedHour}:0${formattedMinute}`;
+    }
+    return `${formattedHour}:${formattedMinute}`;
 }
